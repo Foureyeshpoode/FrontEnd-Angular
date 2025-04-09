efbaa4169f85ab7ccc7f66bf43098b43c9bc8930
unknown <cookeryan087@gmail.com>
2025-04-09 09:44:38 +0000

    Lab 7 Complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Browser } from '@capacitor/browser';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+  location:any = "";
+  latitude:number = 0;
+  longtitude:number = 0;
+
   constructor() {}
+
+  async openBrowser(){
+    await Browser.open({ url: 'http://capacitorjs.com/' });
+  }
+
+  async getLocation(){
+    this.location = await Geolocation.getCurrentPosition();
+    this.latitude = this.location.coords.latitude;
+    this.longtitude = this.location.coords.longtitude;
+    console.log(this.location);
+}
 }
